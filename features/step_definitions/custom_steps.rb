58e6ec33961fd46a71a55a6555e811d0b20b7de3
Lukasz Fryc <fryc.lukasz@gmail.com>
2015-06-13 09:49:45 +0200

    [WIP] Added steps definitions outline using page objects

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -1,15 +1,18 @@
 Given(/^I am on the Welcome screen$/) do
-	pending # express the regexp above with the code you wish you had
+  @app.welcome_screen.await
 end
 
 Given(/^I press Sign In button$/) do
-	pending # express the regexp above with the code you wish you had
+  @app.welcome_screen.touch_sign_in
 end
 
 When(/^I sign in as "(.*?)" with password "(.*?)"$/) do |arg1, arg2|
-	pending # express the regexp above with the code you wish you had
+  @app.sign_in_screen.enter_email(email)
+  @app.sign_in_screen.enter_password(password)
+
+  @app.sign_in_screen.touch_sign_in
 end
 
 Then(/^I should see Cover Stories screen$/) do
-	pending # express the regexp above with the code you wish you had
+  @app.cover_stories_screen.await
 end
