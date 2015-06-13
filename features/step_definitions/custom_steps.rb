bd5242b6dce76484bd530d3fb1879d6321d31ea7
Lukasz Fryc <fryc.lukasz@gmail.com>
2015-06-13 09:28:58 +0200

    [WIP] Added steps definitions outlines generated by Calabash

---
+++ features/step_definitions/custom_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the Welcome screen$/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I press Sign In button$/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+When(/^I sign in as "(.*?)" with password "(.*?)"$/) do |arg1, arg2|
+	pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see Cover Stories screen$/) do
+	pending # express the regexp above with the code you wish you had
+end
