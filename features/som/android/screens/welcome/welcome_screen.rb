47f2540ff5595395861cc109cf7ab2fad6ac8ca6
Lukasz Fryc <fryc.lukasz@gmail.com>
2015-06-13 10:11:04 +0200

    [WIP] Implemented touch_sign_in in Welcome Screen

--- features/som/android/screens/welcome/welcome_screen.rb
+++ features/som/android/screens/welcome/welcome_screen.rb
@@ -7,4 +7,8 @@ class WelcomeScreen
   def await
     @driver.wait_for_element_exists("* marked:'Welcome to Flipboard'")
   end
+
+  def touch_sign_in
+  	@driver.touch("* id:'first_launch_cover_swipe_or_sign_in'")
+  end
 end
