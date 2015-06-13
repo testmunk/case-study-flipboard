d481aaf95003cb2eae0e5b051e15beafca4324c6
Lukasz Fryc <fryc.lukasz@gmail.com>
2015-06-13 10:36:56 +0200

    [WIP] Added Cover Stories screen

--- features/som/android/flipboard_app.rb
+++ features/som/android/flipboard_app.rb
@@ -1,5 +1,6 @@
 require 'som/android/screens/welcome/welcome_screen'
 require 'som/android/screens/sign_in/sign_in_screen'
+require 'som/android/screens/cover_stories/cover_stories_screen'
 
 
 class FlipboardApp
@@ -16,5 +17,10 @@ class FlipboardApp
   def sign_in_screen
     @sign_in_screen ||= SignInScreen.new(@driver)
     @current_screen = @sign_in_screen
+  end
+
+  def cover_stories_screen
+    @cover_stories_screen ||= CoverStoriesScreen.new(@driver)
+    @current_screen = @cover_stories_screen
   end
 end
